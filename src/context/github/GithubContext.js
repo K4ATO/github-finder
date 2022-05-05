5e9c108f61866c8ab167db97d34a919d71530a1f
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 19:14:38 +0200

    use githubReducer rather than useState

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,19 +1,25 @@
-import { createContext, useState } from "react";
-
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
+  const inititalState = {
+    users: [],
+  };
+  const [state, dispatch] = useReducer(githubReducer, inititalState);
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`);
     const data = await response.json();
-    setUsers(data);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
   return (
     <GithubContext.Provider
       value={{
-        users,
+        users: state.users,
         fetchUsers,
       }}
     >
