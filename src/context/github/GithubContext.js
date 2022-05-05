e1952f4e2a949b8ebfa5ffdb0cdde9970b8ab911
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 19:01:52 +0200

    remove api token

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,16 +2,11 @@ import { createContext, useState } from "react";
 
 const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
+    const response = await fetch(`${GITHUB_URL}/users`);
     const data = await response.json();
     setUsers(data);
   };
