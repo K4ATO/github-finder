da05b1f2f347adf45248973d9fb9cc609fcbbf22
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 20:46:26 +0200

    add alert provider to our app

--- src/App.js
+++ src/App.js
@@ -6,23 +6,28 @@ import NavBar from "./components/layout/NavBar";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
+import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./components/layout/Alert";
 const App = () => {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <NavBar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <NavBar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 };
