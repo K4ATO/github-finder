db17c3b39410c44a491a58d6c72c2e3923c106cc
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 18:25:39 +0200

    add userItem component

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import UserItem from "./UserItem";
 const UserResults = () => {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -18,7 +19,7 @@ const UserResults = () => {
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
       {users.map((user) => (
-        <h3>{user.login}</h3>
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
