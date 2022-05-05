d145d88931dbf6b0de5269a19c582a139c178e33
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 19:59:50 +0200

    clear useEffect

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
 import UserItem from "./UserItem";
 const UserResults = () => {
-  const { users, fetchUsers } = useContext(GithubContext);
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { users } = useContext(GithubContext);
+
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
       {users.map((user) => (
