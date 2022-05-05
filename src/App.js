e740e7f2008bfa1b418660c0868fd5ed0ca6ad9c
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 17:24:36 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
