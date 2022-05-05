add411db77f98155d4d1f98deef3b8f9ffbc2eea
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 19:01:39 +0200

    use useContext instead of useState

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useEffect, useContext } from "react";
-import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
+import UserItem from "./UserItem";
 const UserResults = () => {
   const { users, fetchUsers } = useContext(GithubContext);
   useEffect(() => {
     fetchUsers();
   }, []);
-
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
       {users.map((user) => (
