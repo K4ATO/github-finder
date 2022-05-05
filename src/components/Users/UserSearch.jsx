0131125ef82e3b6b47bfeadecfbe02e91ddaac21
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 20:46:06 +0200

    add alert above search

--- src/components/Users/UserSearch.jsx
+++ src/components/Users/UserSearch.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import ALertContext from "../../context/alert/AlertContext";
 const UserSearch = () => {
   const [text, setText] = useState("");
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(ALertContext);
   const handleChange = (e) => {
     setText(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text === "") {
-      alert("please enter somthing");
+      setAlert("Please enter something", "error");
     } else {
       searchUsers(text);
       setText("");
