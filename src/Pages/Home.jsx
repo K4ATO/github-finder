1ec6bf13a481c827098fde3504b5719265fb6ae2
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 20:00:19 +0200

    add userSearch for the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import UserResults from "../components/Users/UserResults";
+import UserSearch from "../components/Users/UserSearch";
 const Home = () => {
   return (
     <>
+      <UserSearch />
       <UserResults />
     </>
   );
