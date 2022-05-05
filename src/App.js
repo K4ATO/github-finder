180e8624327beeb6b01078c48d4452f616676175
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 22:09:12 +0200

    finish the project with github provider

--- src/App.js
+++ src/App.js
@@ -16,16 +16,25 @@ const App = () => {
         <Router>
           <div className="flex flex-col justify-between h-screen">
             <NavBar />
+
             <main className="container mx-auto px-3 pb-12">
-              <Alert />
               <Routes>
-                <Route path="/" element={<Home />} />
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
                 <Route path="/about" element={<About />} />
                 <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
-                <Route path="/*" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
+
             <Footer />
           </div>
         </Router>
