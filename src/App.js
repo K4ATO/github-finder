48bfb7edb3bbf09c49efd7298ae119936316a414
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 17:18:12 +0200

    add footer to our app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">Content</main>
+        <Footer />
       </div>
     </Router>
   );
